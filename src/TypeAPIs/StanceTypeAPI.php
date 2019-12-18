f4cdb87bbb8c9f8a0ff5dde7442090769105958c
Leo <losoviz@gmail.com>
2019-12-18 16:45:34 +0800

    Split `process` into `isInstanceOfType` and `isIDOfType`

--- src/TypeAPIs/StanceTypeAPI.php
+++ src/TypeAPIs/StanceTypeAPI.php
@@ -1,6 +1,7 @@
 <?php
 namespace PoP\StancesWP\TypeAPIs;
 
+use function get_post;
 use WP_Post;
 use PoP\Stances\TypeAPIs\StanceTypeAPIInterface;
 /**
@@ -17,5 +18,31 @@ class StanceTypeAPI implements StanceTypeAPIInterface
     public function isInstanceOfStanceType($object): bool
     {
         return ($object instanceof WP_Post) && $object->post_type == \POP_USERSTANCE_POSTTYPE_USERSTANCE;
+    }
+
+    /**
+     * Get the stance with provided ID or, if it doesn't exist, null
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function getStance($id)
+    {
+        $post = get_post($id);
+        if (!$post || $post->post_type != \POP_USERSTANCE_POSTTYPE_USERSTANCE) {
+            return null;
+        }
+        return $post;
+    }
+
+    /**
+     * Indicate if an stance with provided ID exists
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function stanceExists($id): bool
+    {
+        return $this->getStance($id) != null;
     }
 }
